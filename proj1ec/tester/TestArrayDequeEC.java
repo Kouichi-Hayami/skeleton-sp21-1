4c5b2b4d7e21983f655cd8d150c9eb92a0388620
Kouichi <qiuguapi@gmail.com>
2025-07-03 11:23:19 +0800

    second fix

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,55 +1,58 @@
 package tester;
 
 import static org.junit.Assert.*;
+
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
-
-import java.util.ArrayDeque;
 
 public class TestArrayDequeEC {
 
     @Test
     public void randomizedTestWithDebugLog() {
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-        ArrayDeque<Integer> correct = new ArrayDeque<>();
+        ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
 
         StringBuilder log = new StringBuilder();
 
         for (int i = 0; i < 10000; i++) {
-            int choice = StdRandom.uniform(0, 100);  // Now includes 0–4
+            double choice = StdRandom.uniform();
             int val = StdRandom.uniform(1, 100);
 
-
-            if (choice < 0.33){
+            if (choice < 0.25) {
                 student.addFirst(val);
                 correct.addFirst(val);
                 log.append("addFirst(").append(val).append(")\n");
-            }
-            else if (choice < 0.67){
+
+            } else if (choice < 0.5) {
                 student.addLast(val);
                 correct.addLast(val);
                 log.append("addLast(").append(val).append(")\n");
-            }
-            else {
+
+            } else if (choice < 0.75) {
+                int expectedSize = correct.size();
+                int actualSize = student.size();
+                log.append("size()\n");
+                assertEquals(log.toString(), expectedSize, actualSize);
+
+            } else {
                 int size = student.size();
                 log.append("size()\n");
-                if(size > 0){
+
+                if (size > 0) {
+                    Integer expected, actual;
                     if (val > 50) {
-                        Integer expected = correct.removeFirst();
-                        Integer actual = student.removeFirst();
+                        expected = correct.removeFirst();
+                        actual = student.removeFirst();
                         log.append("removeFirst()\n");
-                        assertEquals(log.toString(), expected, actual);
-                    }
-                    else {
-                        Integer expected = correct.removeLast();
-                        Integer actual = student.removeLast();
+                    } else {
+                        expected = correct.removeLast();
+                        actual = student.removeLast();
                         log.append("removeLast()\n");
-                        assertEquals(log.toString(), expected, actual);
                     }
+                    assertEquals(log.toString(), expected, actual);
                 }
             }
         }
     }
 }
-
