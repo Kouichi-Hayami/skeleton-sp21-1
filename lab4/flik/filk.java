29d092a0e7c701d7520097891cf6551a1062112d
Kouichi <qiuguapi@gmail.com>
2025-07-15 13:12:48 +0800

    Lab4 finished

---
+++ lab4/flik/filk.java
@@ -0,0 +1,28 @@
+package flik;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class filk {
+    @Test
+    public void testSmallNumbers() {
+        for (int i = 0; i < 128; i++) {
+            assertTrue("Failed for i = " + i, Flik.isSameNumber(i, i));
+        }
+    }
+
+    @Test
+    public void testLargeNumbers() {
+        for (int i = 128; i < 200; i++) {
+            assertTrue("Failed for i = " + i, Flik.isSameNumber(i, i));
+        }
+    }
+
+    @Test
+    public void testDifferentNumbers() {
+        assertFalse(Flik.isSameNumber(100, 101));
+    }
+}
+
+
+
